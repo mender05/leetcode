0ee2ff16e198c2409c5ed07fe6b1f55318b29d11
mender05 <ranwangcs@gmail.com>
2015-08-08 22:09:50 +0800

    tab->spaces

--- power_of_two/power_of_two/power_of_two.cpp
+++ power_of_two/power_of_two/power_of_two.cpp
@@ -5,17 +5,17 @@ using std::bitset;
 
 class Solution {
 public:
-	bool isPowerOfTwo(int n) {
-		bitset<sizeof(long long) * 8> i(n);
-		return i.count() == 1 ? true : false;
-	}
+    bool isPowerOfTwo(int n) {
+        bitset<sizeof(long long) * 8> i(n);
+        return i.count() == 1 ? true : false;
+    }
 };
 
 
 int main()
 {   
-	Solution s;
-	bool r = s.isPowerOfTwo(0);
+    Solution s;
+    bool r = s.isPowerOfTwo(0);
     return 0;
 }
 
