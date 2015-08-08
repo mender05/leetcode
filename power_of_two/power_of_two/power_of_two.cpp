dad08a1ca1841a1ecc2a8e244cfcdaf0c5700f97
mender05 <ranwangcs@gmail.com>
2015-08-08 22:02:07 +0800

    new add

---
+++ power_of_two/power_of_two/power_of_two.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+
+#include <bitset>
+using std::bitset;
+
+class Solution {
+public:
+	bool isPowerOfTwo(int n) {
+		bitset<sizeof(long long) * 8> i(n);
+		return i.count() == 1 ? true : false;
+	}
+};
+
+
+int main()
+{   
+	Solution s;
+	bool r = s.isPowerOfTwo(0);
+    return 0;
+}
+
